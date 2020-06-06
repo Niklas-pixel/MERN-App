ee0444c4dffdccff87c5307c95e51fd14dfd02ce
Niklas <niklas.wernfeldt.94@gmail.com>
2020-06-06 13:08:58 +0200

    fixed invalid hook error by fixing webpack

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import Home from "./pages/Home";
-import { Link, Switch, Route, Redirect } from "react-router-dom";
+import { Link, Switch, Route, Redirect, useHistory } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import Platform from "./pages/Platform";
 import { Context } from "./context/Context";
 
 function App() {
   const { isLoggedIn } = useContext(Context);
-
+  const history = useHistory();
+  console.log(isLoggedIn);
   return (
     <div>
       <Link to="/">Home</Link>
@@ -21,7 +22,7 @@ function App() {
           <SignIn />
         </Route>
         <Route exact path="/platform">
-          {!isLoggedIn ? <Redirect to="/signin" /> : ""}
+          {isLoggedIn ? history.push("/platform") : <Redirect to="/signin" />}
           <Platform />
         </Route>
       </Switch>
